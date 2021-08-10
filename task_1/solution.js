27eea353e70467f7a0a377467562dd4508c09e78
Galiya Nikulina <ksemolina@gmail.com>
2021-08-10 09:39:15 +0700

    task 1 successfully completed

--- task_1/solution.js
+++ task_1/solution.js
@@ -9,7 +9,7 @@ function addInCartMessage(title, price) {
     message = message + " ";
     message = message + productPrice;
     message = message + " ";
-    message * message + "теперь в корзине!";
+    message = message + "теперь в корзине!";
     console.log (message);
     // создайте переменную message
     // присвойте переменной message значение переменной productName
@@ -45,12 +45,12 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-    let newSum = oldSum + difference;
-    let newSumText = newSum + "₽";
-
-
+    
     // Задание №1.3. Увеличение суммы в корзине
-
+    let newSum;
+    newSum = oldSum + difference;
+    let newSumText;
+    newSumText = newSum + " " + "₽";
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
