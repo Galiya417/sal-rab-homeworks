235b7c97264343c2cf18a1e1c810167c49f876bd
Galiya Nikulina <ksemolina@gmail.com>
2021-08-10 09:32:39 +0700

    mistake 1 corrected

--- task_1/solution.js
+++ task_1/solution.js
@@ -9,7 +9,7 @@ function addInCartMessage(title, price) {
     message = message + " ";
     message = message + productPrice;
     message = message + " ";
-    message * message + "теперь в корзине!"
+    message * message + "теперь в корзине!";
     console.log (message);
     // создайте переменную message
     // присвойте переменной message значение переменной productName
@@ -47,7 +47,7 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
     let newSum = oldSum + difference;
     let newSumText = newSum + "₽";
-    
+
 
     // Задание №1.3. Увеличение суммы в корзине
 
